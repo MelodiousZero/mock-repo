7f802179bcfb4bfbc41a5fd2789bc92b579bdb5f
Raul Llamosas <raul.llamosas@nielseniq.com>
2022-09-14 23:02:43 +0000

    add code in files types: .py remove code in files types:

--- content.py
+++ content.py
@@ -3,13 +3,12 @@ import git
 from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
-repo = git.Repo(r"C:\Users\llra2001\Documents\GitHub\automation-bigretailers-ecuador")
+repo = git.Repo(r"C:\Users\llra2001\Documents\GitHub\geo-scan-extractions")
 # Your mock repo
 mock_repo = git.Repo(r"C:\Users\llra2001\Documents\GitHub\mock-repo")
 importer = Importer([repo], mock_repo)
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github uses my personal email
 importer.set_author(['raul.llamosas182@gmail.com', 'raul.llamosas@nielseniq.com'])
-print("eqmug")
-print("fwugx")
-print("gqvti")
+importer.import_repository()
+print("wrxxj")
