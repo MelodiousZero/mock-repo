f5efec5f0ee829b670b072a1bf45e276b54a8374
Raul Llamosas <raul.llamosas@nielseniq.com>
2022-09-23 13:50:00 +0000

    add code in files types: .py,.csv,.csv" remove code in files types: .py

---
+++ content.py
@@ -0,0 +1,13 @@
+# Contributions-Importer-For-Github/run_script.py
+import git
+from git_contributions_importer import *
+
+# Your private repo or Bitbucket repo
+repo = git.Repo(r"C:\Users\llra2001\Documents\GitHub\autocoding-e2e")
+# Your mock repo
+mock_repo = git.Repo(r"C:\Users\llra2001\Documents\GitHub\mock-repo")
+importer = Importer([repo], mock_repo)
+# I use both my personal email and work email here,
+# Since the private repo uses work email, and Github uses my personal email
+importer.set_author(['raul.llamosas182@gmail.com', 'raul.llamosas@nielseniq.com'])
+print("tusyb")
