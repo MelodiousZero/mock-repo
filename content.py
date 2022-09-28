6075e00c6fb3aa9674e3719b1c9e94bbc91843a0
Raul Llamosas <raul.llamosas@nielseniq.com>
2022-09-28 13:49:39 +0000

    add code in files types: .csv remove code in files types:

--- content.py
+++ content.py
@@ -3,7 +3,7 @@ import git
 from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
-repo = git.Repo(r"C:\Users\llra2001\Documents\GitHub\sku-links")
+repo = git.Repo(r"C:\Users\llra2001\Documents\GitHub\traffic-lights-validation")
 # Your mock repo
 mock_repo = git.Repo(r"C:\Users\llra2001\Documents\GitHub\mock-repo")
 importer = Importer([repo], mock_repo)
@@ -11,4 +11,3 @@ importer = Importer([repo], mock_repo)
 # Since the private repo uses work email, and Github uses my personal email
 importer.set_author(['raul.llamosas182@gmail.com', 'raul.llamosas@nielseniq.com'])
 importer.import_repository()
-print("qiuag")
