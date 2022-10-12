ee1efb01d656006d3eeb3371c8e68637320a5399
Raul Llamosas <raul.llamosas@nielseniq.com>
2022-10-12 16:59:15 +0000

    add code in files types: .json remove code in files types:

--- content.py
+++ content.py
@@ -3,13 +3,11 @@ import git
 from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
-repo = git.Repo(r"C:\Users\llra2001\Documents\GitHub\traffic-lights-validation")
+repo = git.Repo(r"C:\Users\llra2001\Documents\GitHub\automation-bigretailers-ecuador")
 # Your mock repo
 mock_repo = git.Repo(r"C:\Users\llra2001\Documents\GitHub\mock-repo")
 importer = Importer([repo], mock_repo)
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github uses my personal email
 importer.set_author(['raul.llamosas182@gmail.com', 'raul.llamosas@nielseniq.com'])
-print("nrqae")
-print("vtuhf")
-print("oixsh")
+importer.import_repository()
